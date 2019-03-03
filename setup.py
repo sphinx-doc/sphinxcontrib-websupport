a91bf74331bb093f9c804c16640c06bb0cbf0109
Jon Dufresne <jon.dufresne@gmail.com>
2019-03-03 07:01:30 -0800

    Add Python 3.7 to the tox matrix and document support

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Sphinx',
         'Framework :: Sphinx :: Extension',
         'Topic :: Documentation',
