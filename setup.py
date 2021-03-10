a9f713c67ebfd1298fbdd861082161f646500f8f
Miro Hrončok <miro@hroncok.cz>
2021-03-10 18:40:42 +0100

    Add jinja2 and sphinx to install_requires
    This cannot work without sphinx and it imports from jinja2 directly.

--- setup.py
+++ setup.py
@@ -71,6 +71,8 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
+        'jinja2',
+        'sphinx',
         'sphinxcontrib-serializinghtml',
     ],
     extras_require=extras_require,
